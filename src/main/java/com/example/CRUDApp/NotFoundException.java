930ef7a59f42d0d8f487930bbc5c2bc2f94110d2
laasque <63492956+laasque@users.noreply.github.com>
2021-03-19 01:13:56 +0300

    Update NotFoundException.java

--- src/main/java/com/example/CRUDApp/NotFoundException.java
+++ src/main/java/com/example/CRUDApp/NotFoundException.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// Not found exception class to show not found error
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException{
 
